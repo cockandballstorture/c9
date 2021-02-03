aa77fd0409a23c0620a35f74d105b8c2293f853b
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2021-02-03 23:07:23 +0300

    Create ex0.c

---
+++ ex0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(void) {
+	FILE *f;
+	char a[20];
+	f=fopen("C:\cr.txt","w+");
+	fprintf(f, "what");
+	fclose(f);
+	f=fopen("C:cr.txt","r");
+	fgets(a, 20, f);
+	printf("%s", a);
+	fclose(f);
+}
