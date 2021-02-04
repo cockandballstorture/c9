c7c147005969a71e6eb46bc33487c89f4198ebe2
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2021-02-04 19:53:08 +0300

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int find(char a){
+	char v[30]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    int i;
+	for (i=0;i<25;i++){
+		if(a==v[i]){
+			return(i);
+		}
+	}
+}
+char cz(char a,int w){
+	char v[30]={'a','b','c','d','e','f','g','h','k','i','j','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	if((a >= '0') && (a <='9'))
+	{
+		a=a+w;
+		}
+	else{
+		 a=find(a)+w;
+		 a=a%28;
+		 a=v[a];
+		}
+	return(a);
+}
+void main(void) {
+	FILE *f;
+	char a[30];
+	char x[30];
+	int w;
+	int i;
+	int q=0;
+	f=fopen("C:cr.txt","w+");
+	scanf("%29s", x);
+	scanf("%d", &w);
+	 for (i = 0; x[i] != '\0'; i++)
+	  {
+	    q++;
+	  }
+	for (i=0;i<q;i++){
+		x[i]=cz(x[i],w);
+	}
+	fprintf(f, "%s",x);
+	fclose(f);
+	f=fopen("C:cr.txt","r");
+	fgets(a, 29, f);
+	printf("%s", a);
+	fclose(f);
+}
