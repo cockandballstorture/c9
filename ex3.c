aba91fe58e29a565173069426401a9df970ce5fa
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2021-02-06 01:51:32 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -4,7 +4,7 @@
 int find(char a){
     char v[30]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     int i;
-	for (i=0;i<25;i++){
+	for (i=0;i<26;i++){
 		if(a==v[i]){
 			return(i);
 		}
